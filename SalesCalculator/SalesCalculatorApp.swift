709fc729672480e18c73de7e0ae97673041f19fa
Silvana Tekia <Silvanatekia0@gmail.com>
2024-02-12 13:00:42 +0000

    Initial Commit

---
+++ SalesCalculator/SalesCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesCalculatorApp.swift
+//  SalesCalculator
+//
+//  Created by Silvana Tekia on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SalesCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
