317f75dd7dea344bf5243409bbcccd096e619451
Silvana Tekia <Silvanatekia0@gmail.com>
2024-02-14 16:49:45 +0000

    colour uodated

--- SalesCalculator/ContentView.swift
+++ SalesCalculator/ContentView.swift
@@ -8,16 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   @State var beforeTaxPrice = ""
+    @State var salesTaxRate = ""
+    @State var afterTaxPrice = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Sales Tax Calculator")
+            .font(.largeTitle)
+            .foregroundStyle(Color.blue)
+            
+            HStack{
+                Text("Before Tax Price")
+                    .foregroundStyle(.red)
+                TextField("$0", text: $beforeTaxPrice)
+                    .keyboardType(/*@START_MENU_TOKEN@*/.decimalPad/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.top, 40.0)
+            HStack{
+                Text("Sales Tax Rate")
+                TextField("$0", text: $salesTaxRate)
+                    .keyboardType(/*@START_MENU_TOKEN@*/.decimalPad/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.top, 20.0)
+            
+            HStack{
+                Text("After Tax Price")
+                TextField("$0", text: $afterTaxPrice)
+                    .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+            }
+            .padding(.top, 40.0)
+            Button("Calculate") {
+                if let beforeTax = Float(beforeTaxPrice),
+                   let tax = Float(salesTaxRate) {
+                    let salesTax = beforeTax * tax / 100
+                    afterTaxPrice = String(beforeTax + salesTax)
+                    
+                }
+            }
+            .padding(.top, 40.0)
+            
+            Spacer()
         }
         .padding()
+        
+        }
+      
     }
-}
 
 #Preview {
     ContentView()
